b4a39a01409e614d13f77ab47a2cb801ba294765
sandro <sandroazerty@icloud.com>
2022-07-22 00:07:17 +0200

    add netlify cms

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
     <h1>Hello world</h1>
 </template>
+
+<script setup>
+    useHead({
+        script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+    })
+</script>
